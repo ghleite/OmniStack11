46a9627147ce4b134b837d8b030c8fbdb8a27712
Gabriel Leite <gabrielholeite@gmail.com>
2020-03-28 18:25:10 -0300

    Configurando gitignore e finalizando back-end

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,25 +1,22 @@
 const express = require('express');
-const crypto = require('crypto');
-const connection = require('./database/connection');
+
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
 
-routes.post('/ongs', async (request, response) => {
-    const { name, email, whatsapp, city, uf } = request.body;
+routes.post('/sessions', SessionController.create);
 
-    const id = crypto.randomBytes(4).toString('HEX');
-    console.log(id);
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
 
-    await connection('ongs').insert({
-        id,
-        name,
-        email,
-        whatsapp,
-        city,
-        uf,
-    })
+routes.get('/profile', ProfileController.index);
 
-    return response.json({ id });
-});
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
+
 
 module.exports = routes;
