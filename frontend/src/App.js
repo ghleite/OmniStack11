00e42ed8293641056d6d415819bc1ab55971939c
Gabriel Leite <gabrielholeite@gmail.com>
2020-04-06 21:10:02 -0300

    atualizando projeto

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import Header from './Header';
 
 function App() {
-  const [counter, setCounter] = useState(0);
+  const [counter, setCounter] = useState(1);
 
   function increment() {
     setCounter(counter + 1);
