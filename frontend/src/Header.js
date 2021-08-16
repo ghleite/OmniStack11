b22270457eee17058ba07f6b45d2849140faf9f5
Gabriel Leite <gabrielholeite@gmail.com>
2021-08-16 14:28:37 -0300

    Update Header.js

--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -7,3 +7,6 @@ export default function Header({children}) {
         </header>
     );
 }
+
+https://teams.microsoft.com/l/meetup-join/19%3ameeting_OTEyM2FjM2QtMTRiYi00ZWYxLWIxZmItMWM0YTY3ZDIwYThl%40thread.v2/0?context=%7b%22Tid%22%3a%22f9cfd8cb-c4a5-4677-b65d-3150dda310c9%22%2c%22Oid%22%3a%22b1f17e0e-ed08-4349-b11b-16c499a6764b%22%7d
+
